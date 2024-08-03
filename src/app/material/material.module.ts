0dcc98151b63e371384191b952ec523f93d9a179
Nestor Daniel Gomez <Nestor.Gomez@terceirizadoraizen.com>
2024-08-03 15:43:56 -0300

    Sidenav adn form login-register

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,23 @@
 import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  exports: []
+  exports:[
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+  ]
 })
 export class MaterialModule { }
