1784bfc5ecd1e3682485986faf1f976e3ab2ba28
Nestor Daniel Gomez <Nestor.Gomez@terceirizadoraizen.com>
2024-08-09 16:05:36 -0300

    images and setup pre-crud

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+import { environments } from '../../../environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+  constructor(private httpClient: HttpClient) { }
+
+  private baseUrl : string = environments.baseUrl
+
+  getHeroes():Observable<Hero[]>{
+    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  getHeroById(id:string): Observable<Hero | undefined>{
+    return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+                          .pipe(catchError(error => of(undefined)))
+  }
+
+  getSuggestions(query: string):Observable<Hero[]>{
+    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`)
+  }
+
+}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,23 +1,38 @@
 import { NgModule } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports:[
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
     MatFormFieldModule,
+    MatGridListModule,
     MatIconModule,
     MatInputModule,
-    MatDividerModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatToolbarModule,
     MatListModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule,
   ]
 })
 export class MaterialModule { }
